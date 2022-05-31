14d851aaf60b962f439955f7424dcb9e3c8008d1
ardionoroma <ardionoroma@gmail.com>
2022-05-31 14:11:28 +0700

    fix create data .py

--- backend/serverless/scripts/createdata.py
+++ backend/serverless/scripts/createdata.py
@@ -1,5 +1,19 @@
 import boto3
 import json
+
+
+def close_open_files(file):
+    try:
+        file.close()
+    except Exception as errorFile:
+        print_error(errorFile)
+
+
+def print_error(error):
+    print("Error: {}".format(error))
+    raise e
+
+
 try:
     dynamodb = boto3.client('dynamodb')
 
@@ -8,39 +22,30 @@ try:
         data = f.read()
     try:
         orders = json.loads(data)
-    except:
-        orders = data
-
+    except ValueError as e:
+        print_error(e)
 
     response = dynamodb.batch_write_item(RequestItems=orders)
-    try:
-        f.close()
-    except:
-        pass
-
+    close_open_files(f)
 
     # aws dynamodb batch-write-item --request-items file://backend/serverless/scripts/create-inventory-data.json
     with open("backend/serverless/scripts/create-inventory-data.json") as f:
         data = json.loads(f.read())
     try:
         inventory = json.loads(data)
-    except:
-        inventory = data
+    except ValueError as e:
+        print_error(e)
 
     response = dynamodb.batch_write_item(RequestItems=inventory)
-    try:
-        f.close()
-    except:
-        pass
 
-
-    #  after:deploy:finalize: aws cognito-idp add-custom-attributes --user-pool-id us-east-1_dn9qLGOJ5 --custom-attributes Name=is_admin,AttributeDataType=String,Mutable=true
+    # after:deploy:finalize: aws cognito-idp add-custom-attributes --user-pool-id us-east-1_dn9qLGOJ5
+    # --custom-attributes Name=is_admin,AttributeDataType=String,Mutable=true
     with open("client/src/be-stack.json") as f:
         data = f.read()
     try:
         stack = json.loads(data)
-    except:
-        stack = data
+    except ValueError as e:
+        print_error(e)
 
     cognito = boto3.client('cognito-idp')
     response = cognito.add_custom_attributes(
@@ -53,9 +58,6 @@ try:
             },
         ]
     )
-    try:
-        f.close()
-    except:
-        pass
-except:
+    close_open_files(f)
+except (Exception,):
     pass
